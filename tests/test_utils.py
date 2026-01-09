50049395c7667ed7fe71d0cd61f2cc5dce214e21
lucidrains <lucidrains@gmail.com>
2026-01-09 08:28:42 -0800

    pack with unpack function returned

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,7 +4,8 @@ from torch_einops_utils.torch_einops_utils import (
     exists,
     pad_at_dim,
     pad_left_at_dim,
-    pad_right_at_dim
+    pad_right_at_dim,
+    pack_with_inverse
 )
 
 def test_exist():
@@ -17,3 +18,18 @@ def test_pad_at_dim():
     assert padded.shape == (3, 7, 1)
     assert torch.allclose(padded, pad_right_at_dim(t, 1, dim = 1))
     assert not torch.allclose(padded, pad_left_at_dim(t, 1, dim = 1))
+
+def test_pack_with_inverse():
+    t = torch.randn(3, 12, 2, 2)
+    t, inverse = pack_with_inverse(t, 'b * d')
+
+    assert t.shape == (3, 24, 2)
+    t = inverse(t)
+    assert t.shape == (3, 12, 2, 2)
+
+    u = torch.randn(3, 4, 2)
+    t, inverse = pack_with_inverse([t, u], 'b * d')
+    assert t.shape == (3, 28, 2)
+    t, u = inverse(t)
+    assert t.shape == (3, 12, 2, 2)
+    assert u.shape == (3, 4, 2)
--- torch_einops_utils/torch_einops_utils.py
+++ torch_einops_utils/torch_einops_utils.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import torch
+from torch import is_tensor
 import torch.nn.functional as F
 
 from einops import rearrange, repeat, reduce, pack, unpack
@@ -12,6 +13,9 @@ def exists(v):
 
 def default(v, d):
     return v if exists(v) else d
+
+def first(arr):
+    return arr[0]
 
 # exported functions
 
@@ -33,3 +37,24 @@ def pad_left_at_dim(t, pad: int, **kwargs):
 
 def pad_right_at_dim(t, pad: int, **kwargs):
     return pad_at_dim(t, (0, pad), **kwargs)
+
+# einops pack
+
+def pack_with_inverse(t, pattern):
+    is_one = is_tensor(t)
+
+    if is_one:
+        t = [t]
+
+    packed, packed_shape = pack(t, pattern)
+
+    def inverse(out, inv_pattern = None):
+        inv_pattern = default(inv_pattern, pattern)
+        out = unpack(out, packed_shape, inv_pattern)
+
+        if is_one:
+            out = first(out)
+
+        return out
+
+    return packed, inverse
